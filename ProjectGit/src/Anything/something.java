91faddfb7928ff9ebae6615b4ddedd1cd3bd9f3e
puliomani26@gmail.com <puliomani26@gmail.com>
2018-01-12 12:36:14 +0530

    first commit

---
+++ ProjectGit/src/Anything/something.java
@@ -0,0 +1,9 @@
+package Anything;
+
+public class something {
+public static void main(String[] args) {
+	System.out.println("hello");
+	System.out.println("Heyyaaaa");
+	System.out.println("fature2");
+}
+}
